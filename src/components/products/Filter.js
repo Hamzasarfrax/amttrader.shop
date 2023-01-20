0c6da4d4244fcc95e32b7ff026c35b28785b364f
Hamza <96947306+Hamzasarfrax@users.noreply.github.com>
2023-01-20 16:11:57 +0500

    Update Filter.js

--- src/components/products/Filter.js
+++ src/components/products/Filter.js
@@ -8,13 +8,17 @@ const Filter = () => {
     const [data, setdata] = useState(Json);
     const [first, setfirst] = useState({});
   
-    const [search, setsearch] = useState();
+ const [search, setsearch] = useState('');
     console.log(search)
-    const filter=(x)=>{
-        const filters=data.filter((item)=>item.category ===x);
+   const filter=(x)=>{
+    const filters=data.filter((item)=>item.category ===x);
+    if(filters.length === 0) {
+        setfirst(data);
+    } else {
         setfirst(filters)
-       
-      }
+    }
+}
+
       const filterall=()=>{
        
       }
@@ -60,27 +64,30 @@ const Filter = () => {
   <button class="btn btn_main" type="button" id="button-addon2">search <AiOutlineSearch/></button>
 </div>
 <div className="">
-    {
         data.filter((item)=>{
-            if(search==null){
-                console.log("empty")
-            }
-            else if(search===item.title){
-                return item
-            }
-        }).map((item,index)=>{
-            return (
-                <>
-                    <div key={index}>
-         
-        
-           {item.id}
-           {item.title}
-        </div>
-                </>
-            )
-        })
-    }
+        if(search === ''){
+            console.log("empty")
+        }
+        else if(item.title.includes(search)){
+            return item
+        }
+    }).map((item,index)=>{
+        return (
+            <>
+                <div key={index}>
+                    {item.id}
+                    {item.title}
+                </div>
+            </>
+        )
+    })
+}
+This way, even if the title of the item doesn't match the search term exactly, it will still be displayed if it includes the search term.
+
+
+
+
+
 </div>
 </div>
   </div>
